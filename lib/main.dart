4ba6844ba8dd34d993fa0521c13eb46bb2fcd090
rmakiyama <rmakiyama.io@gmail.com>
2020-03-11 23:05:11 +0900

    Create an ListView

--- lib/main.dart
+++ lib/main.dart
@@ -7,18 +7,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: RandomWords(),
-        ),
-      ),
+      title: 'Startup Name Generator',
+      home: RandomWords(),
     );
   }
 }
--- lib/ui/widged/RandomWords.dart
+++ lib/ui/widged/RandomWords.dart
@@ -7,9 +7,47 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
+  final _words = <WordPair>[];
+  final _biggerFont = const TextStyle(
+    color: Colors.black87,
+    fontSize: 18.0,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    _words.addAll(generateWordPairs().take(25));
+  }
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: _buildWords(_words),
+    );
+  }
+
+  Widget _buildWords(List<WordPair> words) {
+    return ListView.builder(
+      padding: EdgeInsets.all(16.0),
+      itemCount: words.length,
+      itemBuilder: (context, index) {
+        return _buildRow(words[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      onTap: () {
+        print(pair.asPascalCase);
+      },
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
   }
 }
