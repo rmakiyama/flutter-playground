70ec8937b6269698f69eb507ec14b1c5055a1a6f
rmakiyama <rmakiyama.io@gmail.com>
2020-03-14 23:26:41 +0900

    add BottomNavigationBar

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            title: Text("entry"),
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            title: Text("discovery"),
+            icon: Icon(Icons.search),
+          ),
+          BottomNavigationBarItem(
+            title: Text("stats"),
+            icon: Icon(Icons.insert_chart),
+          ),
+        ],
+      ),
     );
   }
 }
