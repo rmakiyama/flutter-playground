c1b0b32fd90448be639cff83ec1cf600afe5942d
rmakiyama <rmakiyama.io@gmail.com>
2020-03-14 23:58:19 +0900

    add navigate to create screen

--- lib/ui/screens/entry_list_screen.dart
+++ lib/ui/screens/entry_list_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutterplayground/ui/screens/create_entry_screen.dart';
 
 class EntryListScreen extends StatelessWidget {
   @override
@@ -9,7 +10,12 @@ class EntryListScreen extends StatelessWidget {
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
-          print("EntryListScreen");
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => CreateEntryScreen(),
+            ),
+          );
         },
         icon: Icon(Icons.add),
         label: Text("add diary"),
